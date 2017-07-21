1efec6f35e4fc7bf3fc3e56e81f52bd08f563b47
Hack Reactor Student <student@hackreactor.com>
2017-07-21 15:34:20 -0500

    initial setup for sprint

--- server/schema.sql
+++ server/schema.sql
@@ -2,15 +2,27 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  user_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  user TEXT(25)
+);
 
+CREATE TABLE rooms (
+  room_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  room TEXT(25)
+);
 
-
-
+CREATE TABLE messages (
+  message_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  message TEXT(25),
+  created_date TIMESTAMP(6),
+  user_id INTEGER(3),
+  room_id INTEGER(2),
+  FOREIGN KEY (user_id) REFERENCES users (user_id),
+  FOREIGN KEY (room_id) REFERENCES rooms (room_id)
+);
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
