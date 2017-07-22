f387288fa2684bb5540817d5a655fb77424a4126
Hack Reactor Student <student@hackreactor.com>
2017-07-22 16:02:30 -0500

    Passed no tests, but full functionality.

--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,17 @@
-var mysql = require('mysql');
+//var mysql = require('mysql');
+var mysql = require('promise-mysql');
 
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 var dbConnect = mysql.createConnection({
+  host: 'localhost',
   user: 'root',
   password: 'plantlife',
   database: 'chat'
 });
-dbConnect.connect();
+//dbConnect.connect();
 
 exports.dbConnection = dbConnect;
+
+
--- server/models/index.js
+++ server/models/index.js
@@ -1,68 +1,97 @@
 var db = require('../db');
-var quer = require('mysql-query-promise');
+var mysql = require('promise-mysql');
 
 module.exports = {
   messages: {
     get: function (req, res) {
-
-      db.dbConnection.query('SELECT * FROM messages', function (error, results, fields) {
-        //console.log(results);
+      var dbConnect = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'plantlife',
+        database: 'chat'
+      }).then(function(conn) {
+        var results = conn.query('SELECT * FROM messages INNER JOIN users ON messages.user_id = users.user_id INNER JOIN rooms ON messages.room_id = rooms.room_id');
+        conn.end();
+        return results;
+      })
+      .then(function(results) {
+        console.log(results);
         var messages = [];
         for (let message of results) {
           messages.push({
             text: message.message,
-            username: 'Bob',
-            roomname: 'lobby',
-            objectId: 1337
+            username: message.user,
+            roomname: message.room,
+            objectId: message.message_id
           });
         }
         res.end(JSON.stringify({'results': messages}));
       });
     }, // a function which produces all the messages
     post: function (req, res) {
-      //quer(qs) .then(function(results), function(err));
       var post = '';
       var queryResult;
+      var finalMessage = {};
+      var connection;
+      var thisQuery = '';
+      
       req.on('data', function(data) {
         post += data;
       });
       req.on('end', function(end) {
-        //console.log(post);
+        var theData = JSON.parse(post); 
         //{"username":"anon","text":"hello world","roomname":"lobby","objectId":1}
-        var finalMessage = {};
-        var theData = JSON.parse(post);
-        var thisQuery = 'SELECT user_id FROM users WHERE user = "' + theData.username + '"';
-        queryResult = quer(thisQuery)
-          .then(function(result) {
-            if (result.length > 0) {
-              finalMessage.userid = result[0].user_id;
-            } else {
-              thisQuery = 'INSERT INTO users (user) VALUES ("' + theData.username + '"")';
-              quer(thisQuery).then(function(result) {
-                finalMessage.userid = result[0].user_id;
-              });
-            }
-          }, function(err) {
-            console.log('There\'s an error mate');
-          })
-          .then(function(result) {
-            thisQuery = 'SELECT room_id FROM rooms WHERE room = "' + theData.roomname + '"';
-            quer(thisQuery)
+      
+        var dbConnect = mysql.createConnection({
+          host: 'localhost',
+          user: 'root',
+          password: 'plantlife',
+          database: 'chat'
+        })
+        .then(function(conn) {
+          connection = conn;
+          thisQuery = 'SELECT user_id FROM users WHERE user = "' + theData.username + '"';
+          queryResult = connection.query(thisQuery);
+          return queryResult;
+        })
+        .then(function(result) {
+          if (result.length > 0) {
+            return result;
+          } else {
+            thisQuery = 'INSERT INTO users (user) VALUES ("' + theData.username + '")';
+            return connection.query(thisQuery)
             .then(function(results) {
-              if (results.length > 0) {
-                finalMessage.roomid = result[0].room_id;
-              } else {
-                thisQuery = 'INSERT INTO rooms (room) VALUES ("' + theData.roomname + '"")';
-                quer(thisQuery).then(function(result) {
-                  finalMessage.roomid = result[0].room_id;
-                });
-              }
-            })
-            .then(function() {
-              thisQuery = 'INSERT INTO messages (message, user_id, room_id) VALUES ("' + theData.text + '", ' + finalMessage.userid + ', ' + finalMessage.userid + ')';
+              return [{'user_id': results.insertId}];
             });
-          });
-      });
+          }
+        }).then(function(result) {
+          return finalMessage.userid = result[0].user_id;
+        })
+        .then(function(result) {
+          thisQuery = 'SELECT room_id FROM rooms WHERE room = "' + theData.roomname + '"';
+          return connection.query(thisQuery);
+        })
+        .then(function(results) {
+          if (results.length > 0) {
+            return results;
+          } else {
+            thisQuery = 'INSERT INTO rooms (room) VALUES ("' + theData.roomname + '")';
+            return connection.query(thisQuery)
+            .then(function(results) {
+              return [{'room_id': results.insertId}];
+            });
+          }
+        })
+        .then(function(result) {
+          return finalMessage.roomid = result[0].room_id;
+        })
+        .then(function(results) {
+          thisQuery = 'INSERT INTO messages (message, user_id, room_id) VALUES ("' + theData.text + '", ' + finalMessage.userid + ', ' + finalMessage.roomid + ')';
+          connection.query(thisQuery);
+          connection.end();
+          res.statusCode = 200;
+          res.end();
+        });
       //first query on user
         //then
           //if user exists,
@@ -77,10 +106,8 @@ module.exports = {
             // we do a create to the db with roomname
             // we get that resulting id
         //then
-          //we create a message using the ID and User data from above
-          
-          
-      
+          //we create a message using the ID and User data from above 
+      });
     } // a function which can be used to insert a message into the database
   },
 
